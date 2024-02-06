d8d8c70ed33d433b5c067f6951c9ae93ac484974
guivicentep <guivicente.gvp@hotmail.com>
2024-02-05 22:42:53 -0300

    first commit

---
+++ src/test/java/br/com/gvp/certification/CertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gvp.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
