657f66e9f75d9015d9ba3236a67a98ec2cf14e41
guivicentep <guivicente.gvp@hotmail.com>
2024-02-07 19:35:23 -0300

    feat: Criando filtro para mostrar somente as informações necessárias

---
+++ src/main/java/br/com/gvp/certification/modules/questions/dto/AlternativesResultDTO.java
@@ -0,0 +1,17 @@
+package br.com.gvp.certification.modules.questions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AlternativesResultDTO {
+    private UUID id;
+    private String description;
+}
